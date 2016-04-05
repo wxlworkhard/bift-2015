b207c32245b0b39350dddb27bb276d5f612e3030
wangxiaolong <491538285@qq.com>
2016-04-05 15:21:56 +0800

    MOD: navbar scroll 后 fixed

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,28 +1,46 @@
 define(function () {
     var selector = {
         pkg: '.home_pkg',
-        wrapper: '.home__wrapper'
+        wrapper: '.home__wrapper',
+        navbar: '.home_nav__navbar'
     };
     var $pkgs = $('.home_pkg');
     var $wrapper = $('.home__wrapper');
+    var $navbar = $('.home_nav__navbar');
+    
+    var navbarOffsetTop = $navbar.offset().top;
     
     animate($pkgs);
     
     $(window).on('scroll', function (e) {
         var scrollTop = $(window).scrollTop();
+        
+        // 视差滚动
         var bgPositionY = - scrollTop / 10;
         $wrapper.css('background-position-y', bgPositionY + 'px');
+
+        if (scrollTop > navbarOffsetTop) {
+            $navbar.addClass('fixed');
+        } else {
+            $navbar.removeClass('fixed');
+        }
+
+
     });
 
     function animate($pkgs) {
         var length = $pkgs.length;
         var index = 0;
+
+        $pkgs.addClass('active');
         
-        setInterval(function () {
-            if (index < length) {
-                $pkgs.eq(index).removeClass('active');
-                index++;
-            }
-        }, 300);   
+        setTimeout(function () {
+            setInterval(function () {
+                if (index < length) {
+                    $pkgs.eq(index).removeClass('active');
+                    index++;
+                }
+            }, 300);
+        }, 1000);
     }
 });
